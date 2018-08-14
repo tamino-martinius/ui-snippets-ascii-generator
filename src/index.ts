ff7ac418ba08277fbed15beef0b838fb9baf98da
Tamino Martinius <dev@zaku.eu>
2018-08-14 18:48:37 +0200

    Start with AsciiGenerator class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World');
+import { GUI } from 'dat.gui'
+declare var dat: any;
+
+class AsciiArtGenerator {
+  settings = {
+    charSet: ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+    charRegions: 1,
+  };
+
+  constructor() {
+    const gui: GUI = new dat.GUI();
+    gui.add(this.settings, 'charSet').onChange(this.analyzeCharSet);
+    gui.add(this.settings, 'charRegions').onChange(this.analyzeCharSet);
+    this.analyzeCharSet();
+  }
+
+  analyzeCharSet() {
+    console.log('Tough work');
+  }
+}
+
+const generator = new AsciiArtGenerator();
+console.log(generator);
+
